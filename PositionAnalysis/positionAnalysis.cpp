05fa77a120a4416eae2647e8d1f50d88bbac73cc
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-04-29 23:27:35 +0200

    using vectors more

--- PositionAnalysis/positionAnalysis.cpp
+++ PositionAnalysis/positionAnalysis.cpp
@@ -14,8 +14,11 @@
 
 
 
-std::vector< std::pair<float, float> > getPedestals( const std::string& fileName );
+std::vector< std::pair<float, float> > getPedestals( const std::string& fileName, const std::string& name );
 std::vector< std::pair<float, float> > getPedestalsHodo( const std::string type, const std::string fileName );
+std::vector<float> subtractPedestals( std::vector<float> raw, std::vector< std::pair<float, float> > pedestals, float nSigma );
+float sumVector( std::vector<float> v );
+bool checkVector( std::vector<float> v, float theMax=4095. );
 float getMeanposHodo( std::vector<float> hodo, std::vector< std::pair<float, float> > pedestals);
 
 
@@ -39,11 +42,18 @@ int main( int argc, char* argv[] ) {
   TTree* tree = (TTree*)file->Get("eventRawData");
 
 
-  std::vector<std::pair<float, float> > pedestals = getPedestals("run_BTF_000002_ped_dqmPlots.root");
+  std::vector<std::pair<float, float> > pedestals = getPedestals("run_BTF_000002_ped_dqmPlots.root", "CEF3RAW_cef3RawSpectrum" );
   std::cout << std::endl;
-  std::cout << "-> Got pedestals: " << std::endl;
+  std::cout << "-> Got pedestals of CeF3: " << std::endl;
   for( unsigned i=0; i<CEF3_CHANNELS; ++i )
-    std::cout << " Channel " << i << ": " << pedestals[i].first << std::endl;
+    std::cout << " CeF3 Channel " << i << ": " << pedestals[i].first << std::endl;
+  std::cout << std::endl;
+
+  std::vector<std::pair<float, float> > pedestals_bgo = getPedestals("run_BTF_000002_ped_dqmPlots.root", "BGORAW_bgoRawSpectrum" );
+  std::cout << std::endl;
+  std::cout << "-> Got pedestals of BGO: " << std::endl;
+  for( unsigned i=0; i<BGO_CHANNELS; ++i )
+    std::cout << " BGO Channel " << i << ": " << pedestals_bgo[i].first << std::endl;
   std::cout << std::endl;
 
   std::vector<std::pair<float, float> > pedestals_hodox = getPedestalsHodo("X", "run_BTF_000002_ped_dqmPlots.root");
@@ -83,6 +93,15 @@ int main( int argc, char* argv[] ) {
   TH1D* h1_cef3_corr_2   = new TH1D("cef3_corr_2",   "", 4097, 0., 4097.);
   TH1D* h1_cef3_corr_3   = new TH1D("cef3_corr_3",   "", 4097, 0., 4097.);
   TH1D* h1_cef3_corr_tot = new TH1D("cef3_corr_tot", "", 200, 0., 4.*4097.);
+
+  TH1D* h1_bgo_corr_0   = new TH1D("bgo_corr_0",   "", 4097, 0., 4097.);
+  TH1D* h1_bgo_corr_1   = new TH1D("bgo_corr_1",   "", 4097, 0., 4097.);
+  TH1D* h1_bgo_corr_2   = new TH1D("bgo_corr_2",   "", 4097, 0., 4097.);
+  TH1D* h1_bgo_corr_3   = new TH1D("bgo_corr_3",   "", 4097, 0., 4097.);
+  TH1D* h1_bgo_corr_4   = new TH1D("bgo_corr_4",   "", 4097, 0., 4097.);
+  TH1D* h1_bgo_corr_5   = new TH1D("bgo_corr_5",   "", 4097, 0., 4097.);
+  TH1D* h1_bgo_corr_6   = new TH1D("bgo_corr_6",   "", 4097, 0., 4097.);
+  TH1D* h1_bgo_corr_7   = new TH1D("bgo_corr_7",   "", 4097, 0., 4097.);
   
   TH1D* h1_xPos_hodo = new TH1D("xPos_hodo", "", nBins, -xMax, xMax);
   TH1D* h1_yPos_hodo = new TH1D("yPos_hodo", "", nBins, -xMax, xMax);
@@ -101,14 +120,19 @@ int main( int argc, char* argv[] ) {
     if( iEntry % 100 == 0 ) std::cout << "Entry: " << iEntry << " / " << nentries << std::endl;
 
 
-    // CeF3 fibres are channels 2-5 of board 1
+    // CeF3 fibres
     std::vector<float> cef3;
     for( unsigned i=0; i<CEF3_CHANNELS; ++i ) cef3.push_back(-1.);
+
+    // BGO
+    std::vector<float> bgo;
+    for( unsigned i=0; i<BGO_CHANNELS; ++i ) bgo.push_back(-1.);
 
     // hodoX
     std::vector<float> hodox;
     for( unsigned i=0; i<HODOX_CHANNELS; ++i ) hodox.push_back(-1.);
 
+    // hodoY
     std::vector<float> hodoy;
     for( unsigned i=0; i<HODOY_CHANNELS; ++i ) hodoy.push_back(-1.);
 
@@ -117,49 +141,46 @@ int main( int argc, char* argv[] ) {
       int board  = adcBoard[i];
       int channel= adcChannel[i];
 
-      if( board==1 ) {
-        if( channel==(CEF3_ADC_START_CHANNEL  ) )  cef3[0] = adcData[i];
-        if( channel==(CEF3_ADC_START_CHANNEL+1) )  cef3[1] = adcData[i];
-        if( channel==(CEF3_ADC_START_CHANNEL+2) )  cef3[2] = adcData[i];
-        if( channel==(CEF3_ADC_START_CHANNEL+3) )  cef3[3] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL  ) ) hodox[0] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL+1) ) hodox[1] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL+2) ) hodox[2] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL+3) ) hodox[3] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL+4) ) hodox[4] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL+5) ) hodox[5] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL+6) ) hodox[6] = adcData[i];
-        if( channel==(HODOX_ADC_START_CHANNEL+7) ) hodox[7] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL  ) ) hodoy[0] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL+1) ) hodoy[1] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL+2) ) hodoy[2] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL+3) ) hodoy[3] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL+4) ) hodoy[4] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL+5) ) hodoy[5] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL+6) ) hodoy[6] = adcData[i];
-        if( channel==(HODOY_ADC_START_CHANNEL+7) ) hodoy[7] = adcData[i];
+      if( board==0 ) {
+        if     ( channel==(BGO_ADC_START_CHANNEL  ) )  bgo[0] = adcData[i];
+        else if( channel==(BGO_ADC_START_CHANNEL+1) )  bgo[1] = adcData[i];
+        else if( channel==(BGO_ADC_START_CHANNEL+2) )  bgo[2] = adcData[i];
+        else if( channel==(BGO_ADC_START_CHANNEL+3) )  bgo[3] = adcData[i];
+        else if( channel==(BGO_ADC_START_CHANNEL+4) )  bgo[4] = adcData[i];
+        else if( channel==(BGO_ADC_START_CHANNEL+5) )  bgo[5] = adcData[i];
+        else if( channel==(BGO_ADC_START_CHANNEL+6) )  bgo[6] = adcData[i];
+        else if( channel==(BGO_ADC_START_CHANNEL+7) )  bgo[7] = adcData[i];
+      } else if( board==1 ) {
+        if     ( channel==(CEF3_ADC_START_CHANNEL  ) )  cef3[0]  = adcData[i];
+        else if( channel==(CEF3_ADC_START_CHANNEL+1) )  cef3[1]  = adcData[i];
+        else if( channel==(CEF3_ADC_START_CHANNEL+2) )  cef3[2]  = adcData[i];
+        else if( channel==(CEF3_ADC_START_CHANNEL+3) )  cef3[3]  = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL  ) ) hodox[0] = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL+1) ) hodox[1] = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL+2) ) hodox[2] = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL+3) ) hodox[3] = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL+4) ) hodox[4] = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL+5) ) hodox[5] = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL+6) ) hodox[6] = adcData[i];
+        else if( channel==(HODOX_ADC_START_CHANNEL+7) ) hodox[7] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL  ) ) hodoy[0] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL+1) ) hodoy[1] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL+2) ) hodoy[2] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL+3) ) hodoy[3] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL+4) ) hodoy[4] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL+5) ) hodoy[5] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL+6) ) hodoy[6] = adcData[i];
+        else if( channel==(HODOY_ADC_START_CHANNEL+7) ) hodoy[7] = adcData[i];
       }
     }
 
 
-    bool vetoEvent = false;
-    for( unsigned i=0; i<cef3.size(); ++i ) {
+    std::vector<float>  bgo_corr = subtractPedestals( bgo , pedestals_bgo, 2. );
+    std::vector<float> cef3_corr = subtractPedestals( cef3, pedestals,     2. );
 
-      if( cef3[i]<0. ) {
-        std::cout << "(Event: " << evtNumber << ") Didn't find data for fibre N." << i << "!" << std::endl;
-        vetoEvent = true;
-        break;
-      }
+    bool cef3_ok = checkVector(cef3);
+    bool cef3_corr_ok = checkVector(cef3_corr);
 
-      if( cef3[i]>=4095. ) {
-        std::cout << "(Event: " << evtNumber << ") Fibre N." << i << " in overflow!" << std::endl;
-        vetoEvent = true;
-        break;
-      }
-
-    }
-
-    if( vetoEvent ) continue;
 
 
     // FIRST GET POSITION FROM HODOSCOPE:
@@ -175,67 +196,72 @@ int main( int argc, char* argv[] ) {
 
 
 
-    // THEN USE CALO DATA:
+    if( cef3_ok ) {
 
-    // subtract pedestals:
-    float cef3_0_corr = ( cef3[0] > pedestals[0].first + 2.*pedestals[0].second ) ? (cef3[0] - pedestals[0].first) : 0.;
-    float cef3_1_corr = ( cef3[1] > pedestals[1].first + 2.*pedestals[1].second ) ? (cef3[1] - pedestals[1].first) : 0.;
-    float cef3_2_corr = ( cef3[2] > pedestals[2].first + 2.*pedestals[2].second ) ? (cef3[2] - pedestals[2].first) : 0.;
-    float cef3_3_corr = ( cef3[3] > pedestals[3].first + 2.*pedestals[3].second ) ? (cef3[3] - pedestals[3].first) : 0.;
+      // THEN USE CeF3 DATA:
 
+      float eTot      = sumVector(cef3);
+      float eTot_corr = sumVector(cef3_corr);
 
-    //   0      1
-    //          
-    //          
-    //   3      2
+      h1_cef3_0->Fill( cef3[0] );
+      h1_cef3_1->Fill( cef3[1] );
+      h1_cef3_2->Fill( cef3[2] );
+      h1_cef3_3->Fill( cef3[3] );
+      h1_cef3_3->Fill( eTot );
 
+      h1_cef3_corr_0->Fill( cef3_corr[0] );
+      h1_cef3_corr_1->Fill( cef3_corr[1] );
+      h1_cef3_corr_2->Fill( cef3_corr[2] );
+      h1_cef3_corr_3->Fill( cef3_corr[3] );
+      h1_cef3_corr_tot->Fill( eTot_corr );
+      
 
-    float xySize = 25.; // in mm
-    float chamfer = 2.1; // in mm
+      if( cef3_corr_ok ) {
 
-    float position = xySize/2. - chamfer/4.;
-
-    float xPosW_0 = cef3_0_corr*(-position);
-    float xPosW_1 = cef3_1_corr*(+position);
-    float xPosW_2 = cef3_2_corr*(+position);
-    float xPosW_3 = cef3_3_corr*(-position);
-
-    float yPosW_0 = cef3_0_corr*(+position);
-    float yPosW_1 = cef3_1_corr*(+position);
-    float yPosW_2 = cef3_2_corr*(-position);
-    float yPosW_3 = cef3_3_corr*(-position);
-
-    float eTot = cef3[0]+cef3[1]+cef3[2]+cef3[3];
-    float eTot_corr = cef3_0_corr+cef3_1_corr+cef3_2_corr+cef3_3_corr;
-
-    float xPos = (xPosW_0+xPosW_1+xPosW_2+xPosW_3)/eTot_corr;
-    float yPos = (yPosW_0+yPosW_1+yPosW_2+yPosW_3)/eTot_corr;
-
-    h1_cef3_0->Fill( cef3[0] );
-    h1_cef3_1->Fill( cef3[1] );
-    h1_cef3_2->Fill( cef3[2] );
-    h1_cef3_3->Fill( cef3[3] );
-    h1_cef3_3->Fill( eTot );
-
-    h1_cef3_corr_0->Fill( cef3_0_corr );
-    h1_cef3_corr_1->Fill( cef3_1_corr );
-    h1_cef3_corr_2->Fill( cef3_2_corr );
-    h1_cef3_corr_3->Fill( cef3_3_corr );
-    h1_cef3_corr_tot->Fill( eTot_corr );
-
-    h1_xPos->Fill( xPos );
-    h1_yPos->Fill( yPos );
-
-    h2_xyPos->Fill( xPos, yPos );
+        //   0      1
+        //          
+        //          
+        //   3      2
 
 
-    // CORRELATIONS BETWEEN CALO AND HODO:
+        float xySize = 25.; // in mm
+        float chamfer = 2.1; // in mm
 
-    h2_correlation_xPos->Fill( xPos, xPos_hodo );
-    h2_correlation_yPos->Fill( yPos, yPos_hodo );
+        float position = xySize/2. - chamfer/4.;
+
+        float xPosW_0 = cef3_corr[0]*(-position);
+        float xPosW_1 = cef3_corr[1]*(+position);
+        float xPosW_2 = cef3_corr[2]*(+position);
+        float xPosW_3 = cef3_corr[3]*(-position);
+
+        float yPosW_0 = cef3_corr[0]*(+position);
+        float yPosW_1 = cef3_corr[1]*(+position);
+        float yPosW_2 = cef3_corr[2]*(-position);
+        float yPosW_3 = cef3_corr[3]*(-position);
+
+
+        float xPos = (xPosW_0+xPosW_1+xPosW_2+xPosW_3)/eTot_corr;
+        float yPos = (yPosW_0+yPosW_1+yPosW_2+yPosW_3)/eTot_corr;
+
+        h1_xPos->Fill( xPos );
+        h1_yPos->Fill( yPos );
+
+        h2_xyPos->Fill( xPos, yPos );
+
+
+        // CORRELATIONS BETWEEN CALO AND HODO:
+  
+        h2_correlation_xPos->Fill( xPos, xPos_hodo );
+        h2_correlation_yPos->Fill( yPos, yPos_hodo );
+
+      } // if cef3_ok
+
+    }
 
   }
 
+
+  std::cout << "-> Events passing overflow cut: " << h1_xPos->GetEntries() << "/" << nentries << " (" << 100.* h1_xPos->GetEntries()/nentries << "%)" << std::endl;
 
   std::string outfileName = "PosAn_" + runName + ".root";
   TFile* outfile = TFile::Open( outfileName.c_str(), "RECREATE" );
@@ -275,13 +301,13 @@ int main( int argc, char* argv[] ) {
 
 
 
-std::vector< std::pair<float, float> > getPedestals( const std::string& fileName ) {
+std::vector< std::pair<float, float> > getPedestals( const std::string& fileName, const std::string& name ) {
 
   TFile* file = TFile::Open(fileName.c_str());
 
   std::vector< std::pair<float, float> > peds;
   for( int i=0; i<CEF3_CHANNELS; ++i ) {
-    TH1D* h1_ped = (TH1D*)file->Get(Form("CEF3RAW_cef3RawSpectrum_%d", i));
+    TH1D* h1_ped = (TH1D*)file->Get(Form("%s_%d", name.c_str(), i));
     std::pair<float, float>  thispair;
     thispair.first  = h1_ped->GetMean();
     thispair.second = h1_ped->GetRMS();
@@ -326,3 +352,44 @@ std::vector< std::pair<float, float> > getPedestalsHodo( const std::string type,
   return peds;
 
 }
+
+
+std::vector<float> subtractPedestals( std::vector<float> raw, std::vector< std::pair<float, float> > pedestals, float nSigma ) {
+
+  std::vector<float> corr;
+
+  for(unsigned i=0; i<raw.size(); ++i ) {
+
+    float iCorr = ( raw[i] > pedestals[i].first + nSigma*pedestals[i].second ) ? (raw[i] - pedestals[i].first) : 0.;
+    corr.push_back( iCorr );
+
+  }
+
+  return corr;
+
+}
+
+
+float sumVector( std::vector<float> v ) {
+
+  float sum=0.;
+  for( unsigned i=0; i<v.size(); ++i ) sum += v[i];
+
+  return sum;
+
+}
+
+
+bool checkVector( std::vector<float> v, float theMax ) {
+
+  bool returnBool = true;
+
+  for( unsigned i=0; i<v.size(); ++i ) {
+    if( v[i]<=0. ) return false;
+    if( v[i]>=theMax ) return false;
+  }
+
+  return returnBool;
+
+}
+
