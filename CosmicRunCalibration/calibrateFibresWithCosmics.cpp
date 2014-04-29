b24a3e76a98018daee03339cec965e43c726187a
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-04-29 15:15:24 +0200

    producing a nice summary plot

--- CosmicRunCalibration/calibrateFibresWithCosmics.cpp
+++ CosmicRunCalibration/calibrateFibresWithCosmics.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 #include <stdlib.h>
 
 #include "TFile.h"
@@ -8,10 +7,12 @@
 #include "TH2D.h"
 #include "TF1.h"
 #include "TCanvas.h"
+#include "TStyle.h"
+#include "TPaveText.h"
+#include "TLegend.h"
 
 #include "TMath.h"
 
-//#include "DrawBase.h"
 
 
 struct FitResults {
@@ -34,7 +35,6 @@ struct FitResults {
 
 
 int NORDERS;
-//double XMAX;
 
 
 Double_t PMTFunction(Double_t *x, Double_t *par)
@@ -75,8 +75,19 @@ Double_t PMTFunction(Double_t *x, Double_t *par)
 
 
 
-//void fitSingleHisto( TFile* file, const std::string& histoName, double xMin );
 FitResults fitSingleHisto( TFile* file, const std::string& histoName, double pedMin, double pedMax, double xMin, double xMax );
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main( int argc, char* argv[] ) {
 
@@ -87,7 +98,6 @@ int main( int argc, char* argv[] ) {
     std::cout << "-> NORDER is set to: " << NORDERS << std::endl;
   }
 
-  //DrawBase* db = new DrawBase("cfwc");
 
 
   TFile* file = TFile::Open("run_BTF_000001_cosmics_dqmPlots.root");
@@ -162,19 +172,125 @@ int main( int argc, char* argv[] ) {
   h1_ped_mu->SetMarkerColor(38);
 
 
+
+  // set the TStyle
+  TStyle* style_ = new TStyle("DrawBaseStyle", "");
+  style_->SetCanvasColor(0);
+  style_->SetPadColor(0);
+  style_->SetFrameFillColor(0);
+  style_->SetStatColor(0);
+  style_->SetOptStat(0);
+  style_->SetTitleFillColor(0);
+  style_->SetCanvasBorderMode(0);
+  style_->SetPadBorderMode(0);
+  style_->SetFrameBorderMode(0);
+  style_->SetPadBottomMargin(0.12);
+  style_->SetPadLeftMargin(0.12);
+  style_->cd();
+
+  // For the canvas:
+  style_->SetCanvasBorderMode(0);
+  style_->SetCanvasColor(kWhite);
+  style_->SetCanvasDefH(600); //Height of canvas
+  style_->SetCanvasDefW(600); //Width of canvas
+  style_->SetCanvasDefX(0); //POsition on screen
+  style_->SetCanvasDefY(0);
+
+  // For the Pad:
+  style_->SetPadBorderMode(0);
+  style_->SetPadColor(kWhite);
+  style_->SetPadGridX(false);
+  style_->SetPadGridY(false);
+  style_->SetGridColor(0);
+  style_->SetGridStyle(3);
+  style_->SetGridWidth(1);
+
+  // For the frame:
+  style_->SetFrameBorderMode(0);
+  style_->SetFrameBorderSize(1);
+  style_->SetFrameFillColor(0);
+  style_->SetFrameFillStyle(0);
+  style_->SetFrameLineColor(1);
+  style_->SetFrameLineStyle(1);
+  style_->SetFrameLineWidth(1);
+
+
+  // Margins:
+  style_->SetPadTopMargin(0.05);
+  style_->SetPadBottomMargin(0.15);//0.13);
+  style_->SetPadLeftMargin(0.15);//0.16);
+  style_->SetPadRightMargin(0.05);//0.02);
+
+  // For the Global title:
+
+  style_->SetOptTitle(0);
+  style_->SetTitleFont(42);
+  style_->SetTitleColor(1);
+  style_->SetTitleTextColor(1);
+  style_->SetTitleFillColor(10);
+  style_->SetTitleFontSize(0.05);
+
+  // For the axis titles:
+
+  style_->SetTitleColor(1, "XYZ");
+  style_->SetTitleFont(42, "XYZ");
+  style_->SetTitleSize(0.05, "XYZ");
+  style_->SetTitleXOffset(1.15);//0.9);
+  style_->SetTitleYOffset(1.4); // => 1.15 if exponents
+
+  // For the axis labels:
+
+  style_->SetLabelColor(1, "XYZ");
+  style_->SetLabelFont(42, "XYZ");
+  style_->SetLabelOffset(0.007, "XYZ");
+  style_->SetLabelSize(0.045, "XYZ");
+
+  // For the axis:
+
+  style_->SetAxisColor(1, "XYZ");
+  style_->SetStripDecimals(kTRUE);
+  style_->SetTickLength(0.03, "XYZ");
+  style_->SetNdivisions(510, "XYZ");
+  style_->SetPadTickX(1); // To get tick marks on the opposite side of the frame
+  style_->SetPadTickY(1);
+
+  style_->cd();
+
+
   TCanvas* c2 = new TCanvas( "c2", "", 600, 600 );
 
   c2->cd();
 
   TH2D* h2_axes = new TH2D("axes", "", 10, -0.5, 3.5, 10, 0., 200.);
   h2_axes->SetXTitle( "Channel Number" );
+  h2_axes->SetYTitle( "ADC Counts" );
   h2_axes->Draw();
+
+  TLegend* legend = new TLegend( 0.4, 0.7, 0.68, 0.9 );
+  legend->SetFillColor(0);
+  legend->SetFillStyle(0);
+  legend->SetTextSize(0.038);
+  legend->AddEntry( h1_Q1, "Q1", "P" );
+  legend->AddEntry( h1_offset, "Offset", "P" );
+  legend->AddEntry( h1_ped_mu, "Pedestal", "P" );
+  legend->Draw("same");
+
+  TPaveText* label_top = new TPaveText(0.4,0.953,0.975,0.975, "brNDC");
+  label_top->SetFillColor(kWhite);
+  label_top->SetTextSize(0.038);
+  label_top->SetTextAlign(31); // align right
+  label_top->SetTextFont(62);
+  label_top->AddText("Cosmic Run");
+  label_top->Draw("same");
+
+  gStyle->SetErrorX(0);
 
   h1_Q1->Draw("P same");
   h1_offset->Draw("P same");
   h1_ped_mu->Draw("P same");
 
-  c2->SaveAs("prova.eps");
+  c2->SaveAs("summaryPlot.eps");
+  c2->SaveAs("summaryPlot.png");
 
 
   return 0;
