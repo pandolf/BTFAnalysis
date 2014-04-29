d60365ff55699f04760d650ba68f64254d82f8a0
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-04-29 16:15:01 +0200

    added pedestals and corr/uncorr pos

--- PositionAnalysis/positionAnalysis.cpp
+++ PositionAnalysis/positionAnalysis.cpp
@@ -9,6 +9,7 @@
 #include "TTree.h"
 
 
+std::vector<float> getPedestals( const std::string& fileName );
 
 
 
@@ -31,6 +32,14 @@ int main( int argc, char* argv[] ) {
   TTree* tree = (TTree*)file->Get("eventRawData");
 
 
+  std::vector<float> pedestals = getPedestals("run_BTF_000002_ped_dqmPlots.root");
+  std::cout << std::endl;
+  std::cout << "-> Got pedestals: " << std::endl;
+  std::cout << " Channel 0: " << pedestals[0] << std::endl;
+  std::cout << " Channel 1: " << pedestals[1] << std::endl;
+  std::cout << " Channel 2: " << pedestals[2] << std::endl;
+  std::cout << " Channel 3: " << pedestals[3] << std::endl;
+  std::cout << std::endl;
 
   UInt_t evtNumber;
   tree->SetBranchAddress( "evtNumber", &evtNumber );
@@ -46,6 +55,11 @@ int main( int argc, char* argv[] ) {
   TH1D* h1_yPos = new TH1D("yPos", "", 100, -100., 100.);
 
   TH2D* h2_xyPos = new TH2D("xyPos", "", 100, -100., 100., 100, -100., 100.);
+
+  TH1D* h1_xPos_corr = new TH1D("xPos_corr", "", 100, -100., 100.);
+  TH1D* h1_yPos_corr = new TH1D("yPos_corr", "", 100, -100., 100.);
+
+  TH2D* h2_xyPos_corr = new TH2D("xyPos_corr", "", 100, -100., 100., 100, -100., 100.);
 
   int nentries = tree->GetEntries();
 
@@ -92,13 +106,31 @@ int main( int argc, char* argv[] ) {
       continue;
     }
 
+
+    // subtract pedestals:
+    float cef3_0_corr = cef3_0 - pedestals[0];
+    float cef3_1_corr = cef3_1 - pedestals[1];
+    float cef3_2_corr = cef3_2 - pedestals[2];
+    float cef3_3_corr = cef3_3 - pedestals[3];
+
+    //   0      1
+    //          
+    //          
+    //   2      3
+
     int xPos = (cef3_0+cef3_2) - (cef3_1+cef3_3);
     int yPos = (cef3_0+cef3_1) - (cef3_2+cef3_3);
 
+    int xPos_corr = (cef3_0_corr+cef3_2_corr) - (cef3_1_corr+cef3_3_corr);
+    int yPos_corr = (cef3_0_corr+cef3_1_corr) - (cef3_2_corr+cef3_3_corr);
+
     h1_xPos->Fill( xPos );
     h1_yPos->Fill( yPos );
+    h1_xPos_corr->Fill( xPos_corr );
+    h1_yPos_corr->Fill( yPos_corr );
 
     h2_xyPos->Fill( xPos, yPos );
+    h2_xyPos_corr->Fill( xPos_corr, yPos_corr );
 
   }
 
@@ -112,8 +144,34 @@ int main( int argc, char* argv[] ) {
   
   h2_xyPos->Write();
   
+  h1_xPos_corr->Write();
+  h1_yPos_corr->Write();
+  
+  h2_xyPos_corr->Write();
+  
   outfile->Close();
 
   return 0;
 
+}
+
+
+
+std::vector<float> getPedestals( const std::string& fileName ) {
+
+  TFile* file = TFile::Open(fileName.c_str());
+
+  TH1D* h1_ped0 = (TH1D*)file->Get("CEF3RAW_cef3RawSpectrum_0");
+  TH1D* h1_ped1 = (TH1D*)file->Get("CEF3RAW_cef3RawSpectrum_1");
+  TH1D* h1_ped2 = (TH1D*)file->Get("CEF3RAW_cef3RawSpectrum_2");
+  TH1D* h1_ped3 = (TH1D*)file->Get("CEF3RAW_cef3RawSpectrum_3");
+
+  std::vector<float> peds;
+  peds.push_back(h1_ped0->GetMean());
+  peds.push_back(h1_ped1->GetMean());
+  peds.push_back(h1_ped2->GetMean());
+  peds.push_back(h1_ped3->GetMean());
+
+  return peds;
+  
 }
